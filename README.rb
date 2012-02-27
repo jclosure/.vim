e41affb758d84b7f94e23d3c9971730836deb6c1
jclosure <jclosure@gmail.com>
2012-02-27 00:49:48 -0600

    filled out README with install info

---
+++ README.rb
@@ -0,0 +1,11 @@
+=My Vim Setup
+
+==Installation
+  $ cd ~
+  $ cp -prv .vim .vim.bak
+  $ cd .vim
+  $ git clone https://jclosure@github.com/jclosure/.vim.git
+
+==Dependencies
+  node.js needs to be in $PATH for js beautification
+  
